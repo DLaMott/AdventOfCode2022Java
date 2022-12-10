42e56baa5fefb4283102e353f07ffc8279954750
GothamsJoker <dylanlamott@gmail.com>
2022-12-10 13:35:55 -0500

    Day 4 set up

---
+++ src/com/xmas/Day4/Day4.java
@@ -0,0 +1,13 @@
+package com.xmas.Day4;
+
+public class Day4 {
+
+    public static void main(String[] args) {
+
+    }
+    // One line contains two pairs each of a certain range. Determine if the pair engulfs the other
+    // Return a sum of how many times this occurs
+
+    // Grab data split by , and individual by -
+    // Inspect the section to determine if engulfed
+}
